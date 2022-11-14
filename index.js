522607e8ff6c271b492e55cc98ea06fbf5c5b715
Eric Lehmann <equilehmann92@gmail.com>
2022-11-14 16:18:15 -0300

    Try to load tables

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const { updateIfNeeded } = require('./src/handlers/dbControl.js');
 const {PORT = 3001} = process.env
 
 // Syncing all the models at once.
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log(`Server listening at ${PORT}`); // eslint-disable-line no-console
   });
-});
+}).then(()=> updateIfNeeded());
